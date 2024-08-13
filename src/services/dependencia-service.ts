e33b291a2a468f3c27d5470b916e6620694d83d9
Matheus de Sousa <88354244+mathensousaa@users.noreply.github.com>
2024-08-13 09:56:23 -0300

    Update dependencia-service.ts

--- src/services/dependencia-service.ts
+++ src/services/dependencia-service.ts
@@ -1,6 +1,6 @@
 import { Tarefa } from "../models";
 
 export class DependenciaService {
-    verificarDependencias(idTarefa: number): Tarefa[];
-    notificarAlteracoes(idTarefa: number): void;
+    verificarDependencias(idTarefa: number): Tarefa[] {};
+    notificarAlteracoes(idTarefa: number): void {};
 }
