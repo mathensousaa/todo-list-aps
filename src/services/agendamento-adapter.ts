eaeb4e24d01c8ad18f00194ba3029952302de2de
Matheus de Sousa <88354244+mathensousaa@users.noreply.github.com>
2024-08-13 09:55:48 -0300

    Update agendamento-adapter.ts

--- src/services/agendamento-adapter.ts
+++ src/services/agendamento-adapter.ts
@@ -2,6 +2,6 @@ import { Tarefa } from "../models";
 import { NotificacaoService } from "./notificacao-service";
 
 export class AgendamentoAdapter implements NotificacaoService {
-  agendarNotificacao(tarefa: Tarefa): void;
-  cancelarNotificacao(tarefa: Tarefa): void;
+  agendarNotificacao(tarefa: Tarefa): void {};
+  cancelarNotificacao(tarefa: Tarefa): void {};
 }
