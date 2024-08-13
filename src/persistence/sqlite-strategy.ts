9e468336a923ec163fc07fa02ec637192297eb8a
mathensousaa <mhenrique.sousa43@gmail.com>
2024-08-12 23:55:38 -0300

    fix: sqlite strategy

--- src/persistence/sqlite-strategy.ts
+++ src/persistence/sqlite-strategy.ts
@@ -1,7 +1,7 @@
 import { EstrategiaPersistencia } from "./estrategia-persistencia";
 
 export class SQLiteStrategy implements EstrategiaPersistencia {
-  salvar(chave: string, valor: any): void;
-  carregar(chave: string): any;
-  remover(chave: string): void;
+  salvar(chave: string, valor: any): void {};
+  carregar(chave: string): any {};
+  remover(chave: string): void {};
 }
