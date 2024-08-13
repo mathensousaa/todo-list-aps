58ffe6b051845276bc758a4e886a661161bf1104
Matheus de Sousa <88354244+mathensousaa@users.noreply.github.com>
2024-08-13 09:57:19 -0300

    Update repeticao-service.ts

--- src/services/repeticao-service.ts
+++ src/services/repeticao-service.ts
@@ -1,6 +1,6 @@
 import { RepeticaoConfig, Tarefa } from "../models";
 
 export class RepeticaoService {
-  configurarRepeticao(tarefa: Tarefa, configuracao: RepeticaoConfig): void;
-  gerarNovasInstancias(tarefa: Tarefa): void;
+  configurarRepeticao(tarefa: Tarefa, configuracao: RepeticaoConfig): void {};
+  gerarNovasInstancias(tarefa: Tarefa): void {};
 }
